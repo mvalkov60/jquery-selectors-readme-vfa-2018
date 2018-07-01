289c152a10b2155c6476a7db6e24cf581a37532d
Martin Valkov <mvalkov60@gmail.com>
2018-07-01 02:27:16 +0000

    automatically backed up by learn

---
+++ js/selectors.js
@@ -0,0 +1,15 @@
+function paragraphSelector() {
+  return $("p");
+};
+$(document).ready(function lastImageSelector() {
+  return $("img:last");
+});
+$(document).ready(function ninjaBabySelector() {
+  return $('#baby-ninja');
+});
+$(document).ready(function divSelector() {
+  return $('.pics');
+});
+$(document).ready(function firstListItem() {
+  $('pics pic_list:first-child')
+});
